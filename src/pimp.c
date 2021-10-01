d3164f2d8e4111a233af9fcec2c03529b4f34a87
fhchl <fhchl@users.noreply.github.com>
2021-10-01 12:15:21 +0200

    force symmetry in RLS

--- src/pimp.c
+++ src/pimp.c
@@ -137,29 +137,35 @@ void mat_vec_mul(size_t n, pfloat y[n], pfloat* A[n], pfloat x[n]) {
 }
 
 void rls_update(RLSFilter* self, pfloat x[self->len], pfloat e) {
+    size_t n = self->len;
+
     // P <- alpha^2 P + I q
-    for (size_t i = 0; i < self->len; i++) {
-        for (size_t j = 0; j < self->len; j++)
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++)
             self->P[i][j] *= self->alpha * self->alpha;
         self->P[i][i] += self->q;
     }
     // Px <- P x  // TODO: BLIS
-    mat_vec_mul(self->len, self->Px, self->P, x);
+    mat_vec_mul(n, self->Px, self->P, x);
     // norm <- (x^T P x + I r)
     pfloat norm = self->r;
-    for (size_t i = 0; i < self->len; i++)
+    for (size_t i = 0; i < n; i++)
         norm += self->Px[i] * x[i];
     // k <- P x (x^T P x + I r)^-1
     pfloat eps = 1e-8;
-    for (size_t i = 0; i < self->len; i++)
+    for (size_t i = 0; i < n; i++)
         self->k[i] = self->Px[i] / (norm + eps);
     // w <- w + k e
-    for (size_t i = 0; i < self->len; i++)
+    for (size_t i = 0; i < n; i++)
         self->w[i] += self->k[i] * e;
     // P <- P - k  x^T P  // TODO: BLIS
-    for (size_t i = 0; i < self->len; i++)
-        for (size_t j = 0; j < self->len; j++)
+    for (size_t i = 0; i < n; i++)
+        for (size_t j = 0; j < n; j++)
             self->P[i][j] -= self->k[i] * self->Px[j];
+    // force symmetry: P = (P + P^T)/2
+    for (size_t i = 0; i < n; i++)
+        for (size_t j = 0; j < n; j++)
+            self->P[i][j] = (self->P[i][j] + self->P[j][i]) / 2;
 }
 
 pfloat rls_predict(RLSFilter* self, pfloat x[self->len]) {
